58ddc5c06935e17cee85aacb096bf3dd5c7c10cd
AryaShakya1 <arya@ramailo.tech>
2024-06-11 13:54:36 +0545

    Define methods for Review model

--- app/models/review.py
+++ app/models/review.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+from app.models.book import Book
+
 from .base import BaseModel
+from django.contrib.auth.models import User
 
 
 class Review(BaseModel):
-    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+
+    def __str__(self):
+        return f"{self.content} - {self.book.title} by {self.user}"
+
+    @classmethod
+    def get_all_reviews(cls):
+        return cls.objects.all()
