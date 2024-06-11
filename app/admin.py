00bca7780fc0419fe74a849845560ae8d2efe196
AryaShakya1 <arya@ramailo.tech>
2024-06-11 13:55:15 +0545

    Register models in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from app.models.book import Book
+from app.models.review import Review
+
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("idx", "title", "author", "publication_date")
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("idx", "content", "user", "book")
