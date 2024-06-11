f31d9b85f2cbe5ea8bfc80a788d30006db32166d
AryaShakya1 <arya@ramailo.tech>
2024-06-11 13:55:32 +0545

    Define services for Book

---
+++ app/services/book_service.py
@@ -0,0 +1,21 @@
+from app.models.book import Book
+from app.serializers.book_serializer import BookSerializer
+
+
+class BookService:
+
+    def get_all_books(self):
+        books = Book.get_all_books()
+        return BookSerializer(books, many=True).data
+
+    def get_book_by_id(self, id):
+        book = Book.get_book_by_id(id=id)
+        if not book:
+            return None
+        return BookSerializer(book).data
+
+    def get_book_by_id(self, title):
+        book = Book.get_book_by_title(title=title)
+        if not book:
+            return None
+        return BookSerializer(book).data
