0898434735d4eb5f62f037d5f7d3eead0846ab15
AryaShakya1 <arya@ramailo.tech>
2024-06-12 13:56:02 +0545

    Add services to edit and delete reviews

--- app/services/review_service.py
+++ app/services/review_service.py
@@ -1,5 +1,6 @@
 from app.models.review import Review
 from app.serializers.review_serializer import ReviewSerializer
+from django.core.exceptions import PermissionDenied
 
 
 class ReviewService:
@@ -14,3 +15,32 @@ class ReviewService:
             serializer.save()
             return serializer.data, None
         return None, serializer.errors
+
+    def get_review_by_id(self, id):
+        review = Review.get_review_by_id(id=id)
+        if not review:
+            return None
+        return ReviewSerializer(review).data
+
+    def update_review(self, review_id, data, user):
+        review = Review.get_review_by_id(id=review_id)
+        if not review:
+            return None, "Review not Found"
+        if review.user != user:
+            raise PermissionDenied("You do not have permission to edit this review.")
+        serializer = ReviewSerializer(review, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, None
+        return None, serializer.errors
+
+    def delete_review(self, review_id, user):
+        review = Review.get_review_by_id(id=review_id)
+        if not review:
+            return None, "Review not Found"
+        if review.user != user:
+            raise PermissionDenied("You do not have permission to edit this review.")
+        if not review:
+            return False
+        review.delete()
+        return True
