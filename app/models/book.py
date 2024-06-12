425f5d1541cd65cea9ea52bdfc40f42df64143d6
AryaShakya1 <arya@ramailo.tech>
2024-06-12 13:29:37 +0545

    Add created by field in Books

--- app/models/book.py
+++ app/models/book.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from .base import BaseModel
+from django.contrib.auth.models import User
 
 
 class Book(BaseModel):
@@ -8,6 +9,7 @@ class Book(BaseModel):
     cover_image = models.ImageField(upload_to="cover_images/")
     author = models.CharField(max_length=255)
     publication_date = models.DateTimeField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
         return f"{self.title} - {self.author}"
