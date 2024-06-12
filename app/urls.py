19845cd7cc3b46ba43414c41bd5c6e6b7ab9736a
AryaShakya1 <arya@ramailo.tech>
2024-06-12 15:32:13 +0545

    Add URLs for review update and delete

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 
 from app.views.book import BookUpdateDeleteView, BookView
-from app.views.review import ReviewView
+from app.views.review import ReviewUpdateDeleteView, ReviewView
 from app.views.user import login
 
 urlpatterns = [
     path("v1/books/", BookView.as_view(), name="book"),
-    path('v1/books/<uuid:book_id>/', BookUpdateDeleteView.as_view(), name='book-update-delete'),
+    path(
+        "v1/books/<uuid:book_id>/",
+        BookUpdateDeleteView.as_view(),
+        name="book-update-delete",
+    ),
     path("v1/reviews/", ReviewView.as_view(), name="review"),
+    path(
+        "v1/reviews/<uuid:review_id>/",
+        ReviewUpdateDeleteView.as_view(),
+        name="review-update-delete",
+    ),
     path("v1/user/login/", login, name="login"),
 ]
