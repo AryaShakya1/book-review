97d4fcca136c5d74b4cf6a2a7689e704c1e7d1ed
AryaShakya1 <arya@ramailo.tech>
2024-06-13 10:15:32 +0545

    Create a serializer for book details

--- app/serializers/book_serializer.py
+++ app/serializers/book_serializer.py
@@ -1,10 +1,34 @@
 from rest_framework import serializers
 
 from app.models.book import Book
+from app.serializers.review_serializer import ReviewSerializer
 
 
 class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
-        fields = ["idx", "title", "cover_image", "author", "publication_date","created_by"]
+        fields = [
+            "idx",
+            "title",
+            "cover_image",
+            "author",
+            "publication_date",
+            "created_by",
+        ]
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Book
+        fields = [
+            "idx",
+            "title",
+            "cover_image",
+            "author",
+            "publication_date",
+            "created_by",
+            "reviews",
+        ]
