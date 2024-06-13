de67af1ac6a0662c552d629aec1bb23d6e711ec1
AryaShakya1 <arya@ramailo.tech>
2024-06-13 07:12:19 +0545

    Add exception handling

--- app/views/user.py
+++ app/views/user.py
@@ -8,26 +8,37 @@ from app.builders.response_builder import ResponseBuilder
 @api_view(["POST"])
 def login(request):
     response_builder = ResponseBuilder()
-    username = request.data.get("username")
-    password = request.data.get("password")
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        refresh = RefreshToken.for_user(user)
-        response_object = {
-            "refresh": str(refresh),
-            "access": str(refresh.access_token),
-        }
+    try:
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            response_object = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+            return (
+                response_builder.result_object(response_object)
+                .success()
+                .ok_200()
+                .get_response()
+            )
+        else:
+            return (
+                response_builder.result_object({"message": "Invalid credentials"})
+                .fail()
+                .bad_request_400()
+                .message("Bad Request")
+                .get_response()
+            )
+    except Exception as e:
+        print(f"User login :: exception:: {e}")
         return (
-            response_builder.result_object(response_object)
-            .success()
-            .ok_200()
-            .get_response()
-        )
-    else:
-        return (
-            response_builder.result_object({"message": "Invalid credentials"})
+            response_builder.result_object({"message": "Unable to login user"})
             .fail()
-            .bad_request_400()
-            .message("Bad Request")
+            .internal_error_500()
+            .message("Internal Error")
             .get_response()
         )
+
